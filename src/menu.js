5387399199f970bab8ae2bfc5f22fc9830a7b4fe
Debopri-yo <ghoshlipika53@gmail.com>
2025-09-28 04:03:53 +0530

    finish the menu page and add track the images folder

--- src/menu.js
+++ src/menu.js
@@ -1,18 +1,28 @@
 import "./styles/menu.css";
 import revivalCola from "./images/senkucola.png";
+import ramen from "./images/Ramen.webp";
+import spicycola from "./images/spicycola.png";
+import cinderella from "./images/cinderella.png";
+import tealatte from "./images/tealatte.png";
+import tonic from "./images/tonic.png";
+import Sunshine from "./images/2ssunshine.png";
 export default function loadMenu() {
     const content = document.getElementById("content");
     content.innerHTML = "";
     const menuDiv = document.createElement("div");
     menuDiv.id = "menu";
     const menuItems=[
-        { name: "Senku’s Ramen", description: "The legendary ramen that revived the village’s spirit.", Image: revivalCola },
-        { name: "Revival Cola", description: "Cola is science — fizzy and refreshing!" },
-        { name: "Chrome’s Crystal Candy", description: "Sweet, colorful crystals from the lab." }
-    ];
+        { name: "Senku’s Ramen", description: "The legendary ramen that revived the village’s spirit.", Image: ramen },
+        { name: "Revival Cola", description: "Cola is science — fizzy and refreshing!", Image: revivalCola },
+        { name: "Gen’s Spicy Cola", description:" It's Coca-Cola for adults?A cola with a kick, just like Gen himself.", Image: spicycola },
+        { name: "Kohaku’s Piquant Cinderella", description: "Taste like a sparkling sunset!", Image:cinderella },
+        { name: "Three Layer Tea Latte", description: "A perfect trio of matcha whipped cream, milk and hojicha syrup presented in the cleanest aesthetic.", Image: tealatte },
+        { name: "Tsukasa’s Grenadine & Tonic", description: "Fruity flavor of pomegrante counterbalanced with the lsight bitterness of tonic water. So refreshing!", Image: tonic},
+        { name: "Suika’s Two Side Sunshine", description: "Light, fun and cute. The perfect representation of Suika!", Image: Sunshine },
+    ]
     for (let item of menuItems) {
         const itemCard = document.createElement("div");
-        itemCard.class="itemCard";
+        itemCard.className="itemCard";
         const title = document.createElement("h3");
         title.textContent = item.name;
         const desc = document.createElement("p");
@@ -23,6 +33,7 @@ export default function loadMenu() {
             const img = document.createElement("img");
             img.src = item.Image;
             img.alt = item.name;
+            img.className = "menuImage";
             itemCard.appendChild(img);
         }
         menuDiv.appendChild(itemCard);
